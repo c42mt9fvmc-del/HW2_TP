c575b13eff86afe18a401e1533193c14b04d6611
c42mt9fvmc-del <c42mt9fvmc@privaterelay.appleid.com>
2026-06-03 19:44:19 +0300

    Добавил класс Recipe

--- recipes.py
+++ recipes.py
@@ -25,3 +25,44 @@ class Ingredient:
         if not isinstance(other, Ingredient):
             return False
         return self.name == other.name and self.unit == other.unit
+    
+class Recipe:
+    def __init__(self, title: str, ingredients=None):
+        self.title = title
+        self.ingredients = ingredients if ingredients is not None else []
+
+    def add_ingredient(self, ingredient: Ingredient):
+        for existing in self.ingredients:
+            if existing == ingredient:
+                existing.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio):
+        return isinstance(ratio, (int, float)) and ratio > 0
+
+    def scale(self, ratio: float):
+        if not Recipe.is_valid_ratio(ratio):
+            raise ValueError("Некорректный коэффициент")
+
+        new_ingredients = []
+        for ing in self.ingredients:
+            new_ingredient = Ingredient(
+            ing.name,
+            ing.quantity * ratio,
+            ing.unit
+            )
+            new_ingredients.append(new_ingredient)
+            
+
+        return Recipe(self.title, new_ingredients)
+
+    def __len__(self):
+        return len(self.ingredients)
+
+    def __str__(self):
+        ingredients_text = "\n".join(
+            str(ingredient) for ingredient in self.ingredients
+        )
+        return f"{self.title}\nИнгредиенты:\n{ingredients_text}"
